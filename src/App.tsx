1407ce9e326831fc475f9de572311b7dece6b211
Felipe Soares <felipe8297@gmail.com>
2025-06-06 12:14:18 -0300

    feat: update app title to 'Hello World V11' and add status message for auto-update functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import "./App.css";
 import { runUpdater } from "./utils/updater";
 
 function App() {
-
   useEffect(() => {
     runUpdater();
   }, []);
 
   return (
     <main className="container">
-      <h1>Hello World V8</h1>
+      <h1>Hello World V11</h1>
+      <p>Funciona Auto Update</p>
       <button onClick={runUpdater}>Update</button>
     </main>
   );
