80f26519d0d5cf3b03e8e604db408c1fa71c5ad2
Felipe Soares <felipe8297@gmail.com>
2025-06-09 11:48:20 -0300

    fix: update version numbers to 1.0.14 in package.json, Cargo.toml, Cargo.lock, and tauri.conf.json; modify App component title and remove unused updater code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-// import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  // useEffect(() => {
-  //   console.log("Checking for updates...");
-  //   runUpdater().catch((error) => {
-  //     console.error("Error checking for updates:", error);
-  //   });
-  // }, []);
-
   return (
     <main className="container">
-      <h1>Hello World New Version</h1>
+      <h1>Hello World New Version 14</h1>
       <p>Funciona Auto Update</p>
-      {/* <button
-        onClick={() => {
-          console.log("Manual update check...");
-          runUpdater().catch((error) => {
-            console.error("Error checking for updates:", error);
-          });
-        }}
-      >
-        Check for Updates
-      </button> */}
     </main>
   );
 }
